69632c6f8d7cbc48a8ea56c169614c8a6d3a9a8c
Linsey Briley <linseybriley@gmail.com>
2020-12-01 14:01:58 -0500

    final touches adding jwt and postman collection

--- starter_code/backend/src/auth/auth.py
+++ starter_code/backend/src/auth/auth.py
@@ -99,6 +99,8 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
+    # most of the code in this method was derived from the lectures and examples
+
     # get key from auth0
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
